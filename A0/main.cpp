55d24d2c533f67da0df0596206922c097d857b4e
Kreap-0 <1493025761@qq.com>
2024-10-17 19:23:35 +0800

    A0

--- A0/main.cpp
+++ A0/main.cpp
@@ -5,5 +5,12 @@
 
 int main()
 {
-    Eigen::AngleAxis rotation_vector();
+    Eigen::Vector2d P(2.0, 1.0);
+    Eigen::Affine2d A;
+
+    A.setIdentity();
+    A.translate(Eigen::Vector2d(1.0, 2.0));
+    A.rotate(-45 * M_PI / 180);
+
+    std::cout << (A * P);
 }
